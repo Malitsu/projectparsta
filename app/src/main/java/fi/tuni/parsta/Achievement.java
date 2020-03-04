f0cf49dfc4a970d4f54a75e66b8e988dea5dd41c
Maija <maija.visala@tuni.fi>
2020-03-04 09:42:00 +0200

    Add new activity for achievents.

---
+++ app/src/main/java/fi/tuni/parsta/Achievement.java
@@ -0,0 +1,13 @@
+package fi.tuni.parsta;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Achievement extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_achievement);
+    }
+}
