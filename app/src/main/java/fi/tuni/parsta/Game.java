4c23c283d5745ca24b9d3bbd9bc589806babe526
Malitsu <40868906+malitsu@users.noreply.github.com>
2020-03-04 10:39:34 +0200

    Add palceholder image to git. Start building basic game loop.

--- app/src/main/java/fi/tuni/parsta/Game.java
+++ app/src/main/java/fi/tuni/parsta/Game.java
@@ -6,10 +6,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Game extends AppCompatActivity {
 
+    int rightAnswers = 0;
+    int totalAnswers = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
     }
+
+    protected void gameLoop() {
+        while (totalAnswers <= 20) {
+            boolean rightanswer = getnewQuestion();
+            if(rightanswer) {
+                rightAnswers++;
+            }
+            totalAnswers++;
+        }
+        endRound();
+    }
+
+    protected void endRound() {}
+
+    protected boolean getnewQuestion() {
+        return false;
+    }
+
+
+
 }
 
