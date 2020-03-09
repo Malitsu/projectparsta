d411def96f8daeb9f96178634e2e70ea4ad4c115
Maija <maija.visala@tuni.fi>
2020-03-09 15:29:15 +0200

    Add classes for question and answer option.
    Question object contains information about the image, answer options, right answer, and the number of answer options.
    At this point, the answer option object only contains the written answer option and id.

---
+++ app/src/main/java/fi/tuni/parsta/AnswerOption.java
@@ -0,0 +1,6 @@
+package fi.tuni.parsta;
+
+public class AnswerOption {
+    int id;
+    String content;
+}
---
+++ app/src/main/java/fi/tuni/parsta/Question.java
@@ -0,0 +1,52 @@
+package fi.tuni.parsta;
+
+import java.util.ArrayList;
+
+public class Question {
+    int id;
+    String img_id;
+    int numberOfOptions;
+    ArrayList<AnswerOption> answerOptions;
+
+    public Question(){
+
+    }
+
+    public Question(int id, String img_id, int numberOfOptions){
+        setId(id);
+        setImg_id(img_id);
+        setNumberOfOptions(numberOfOptions);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getImg_id() {
+        return img_id;
+    }
+
+    public void setImg_id(String img_id) {
+        this.img_id = img_id;
+    }
+
+    public int getNumberOfOptions() {
+        return numberOfOptions;
+    }
+
+    public void setNumberOfOptions(int numberOfOptions) {
+        this.numberOfOptions = numberOfOptions;
+    }
+
+    public ArrayList<AnswerOption> getAnswerOptions() {
+        return answerOptions;
+    }
+
+    public void setAnswerOptions(ArrayList<AnswerOption> answerOptions) {
+        this.answerOptions = answerOptions;
+    }
+}
