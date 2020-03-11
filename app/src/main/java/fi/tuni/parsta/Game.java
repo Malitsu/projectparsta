6bb360794d74e28168e39f133bfd3c2e3f2b7063
Hanna Tuominen <hanna.h.tuominen@tuni.fi>
2020-03-11 11:09:09 +0200

    Moved json file to assets. Updated GameImage array to work with convertJsonToGameImageObjects class.
    ConvertJsonToGameImageObjects grt the faces.json file and converts it to a GameImage array with gson.

--- app/src/main/java/fi/tuni/parsta/Game.java
+++ app/src/main/java/fi/tuni/parsta/Game.java
@@ -1,11 +1,25 @@
 package fi.tuni.parsta;
 
+import android.content.Context;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.TypedValue;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,11 +28,19 @@ public class Game extends AppCompatActivity {
 
     int rightAnswers = 0;
     int totalAnswers = 0;
+    private Gson gson;
+    GameImage[] images;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gson = gsonBuilder.create();
+        convertJsonToGameImageObjects();
+        for(GameImage i : images) {
+            Log.d("GAMEIMAGETAG",i.name);
+        }
 
 //        String resourceId = "@drawable/happy_placeholder";
 //
@@ -50,6 +72,17 @@ public class Game extends AppCompatActivity {
             totalAnswers++;
         }
         endRound();
+    }
+
+    public  void  convertJsonToGameImageObjects() {
+        try {
+            String hellou = Util.readFile("faces.json",this);
+            Log.d("GAMEHELLO",hellou);
+            images = gson.fromJson(hellou, GameImage[].class);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     protected void endRound() {}
