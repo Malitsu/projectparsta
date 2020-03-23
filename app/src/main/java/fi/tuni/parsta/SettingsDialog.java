d6dbe04119c41818402a330d2118b16c5e48403c
Hanna Tuominen <hanna.h.tuominen@tuni.fi>
2020-03-23 13:00:57 +0200

    Settings now save the picked sound option answer.

--- app/src/main/java/fi/tuni/parsta/SettingsDialog.java
+++ app/src/main/java/fi/tuni/parsta/SettingsDialog.java
@@ -1,7 +1,7 @@
 package fi.tuni.parsta;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Debug;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,19 +11,24 @@ import android.widget.TextView;
 
 import androidx.fragment.app.DialogFragment;
 
+import static android.content.Context.MODE_PRIVATE;
+
 public class SettingsDialog extends DialogFragment {
     private CheckBox soundSettings;
     private TextView mActionOk, mActionCancel;
     private boolean soundEffectsOn;
+    SharedPreferences sharedPreferences;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.dialog_settings,container,false);
+        sharedPreferences = getContext().getSharedPreferences("settings", MODE_PRIVATE);
+
         mActionCancel = view.findViewById(R.id.action_cancel);
         mActionOk = view.findViewById(R.id.action_ok);
         soundSettings = view.findViewById(R.id.sound_effect_on);
         //get this from saved memory when that is created.
-        soundEffectsOn = true;
+        soundEffectsOn = getSettingsPrefs("sound");
         //set the checkbox to the past settings (true or false).
         soundSettings.setChecked(soundEffectsOn);
 
@@ -38,12 +43,22 @@ public class SettingsDialog extends DialogFragment {
             @Override
             public void onClick(View view) {
                 //save this to memory when ever that option been created
-                soundEffectsOn = soundSettings.isChecked();
+                updateSettingsPrefs("sound",soundSettings.isChecked());
                 Log.d("SettingsDialog","Clicked on sound effect settings and it is currently: " + soundEffectsOn);
                 getDialog().dismiss();
             }
         });
         return view;
+    }
+
+    private void updateSettingsPrefs(String field, Boolean value) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean(field, value);
+        editor.apply();
+    }
+
+    private Boolean getSettingsPrefs(String field) {
+        return sharedPreferences.getBoolean(field, true);
     }
 
 }
