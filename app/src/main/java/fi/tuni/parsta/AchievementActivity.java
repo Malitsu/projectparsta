065d3b95b663665cb34540569eb638d57624bfd9
Maija <maija.visala@tuni.fi>
2020-03-30 11:24:16 +0300

    Add layouts for (achievements) card view and card item.

--- app/src/main/java/fi/tuni/parsta/AchievementActivity.java
+++ app/src/main/java/fi/tuni/parsta/AchievementActivity.java
@@ -13,13 +13,13 @@ public class AchievementActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_achievement);
-        GridLayout gridLayout = findViewById(R.id.achievement_grid);
-        Achievement[] achievements = ProgressController.getAchievements(this);
-
-        for (Achievement a : achievements) {
-            ImageView image = new ImageView(this);
-            image.setImageResource(Util.getStringResourceByName(a.getIcon(), this));
-            gridLayout.addView(image);
-        }
+//        GridLayout gridLayout = findViewById(R.id.achievement_grid);
+//        Achievement[] achievements = ProgressController.getAchievements(this);
+//
+//        for (Achievement a : achievements) {
+//            ImageView image = new ImageView(this);
+//            image.setImageResource(Util.getStringResourceByName(a.getIcon(), this));
+//            gridLayout.addView(image);
+//        }
     }
 }
