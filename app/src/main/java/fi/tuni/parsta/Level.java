0d87b55a44e32f045806d3799c012d3982f2a57c
Hanna Tuominen <hanna.h.tuominen@tuni.fi>
2020-03-31 17:25:45 +0300

    Update Level class with more of needed info for tracking current level.

--- app/src/main/java/fi/tuni/parsta/Level.java
+++ app/src/main/java/fi/tuni/parsta/Level.java
@@ -1,15 +1,50 @@
 package fi.tuni.parsta;
 
+import android.util.Log;
+
+import java.util.ArrayList;
+
 public class Level {
     int level;
     int score;
+    int currentLevelClicks;
 
-    public Level() {
-    }
+    boolean[] areAnswersCorrect;
 
-    public Level(int level, int score) {
+    public Level(int level, int score, int amountOfPictures, int currentLevelClicks) {
         this.level = level;
         this.score = score;
+        this.currentLevelClicks = currentLevelClicks;
+        areAnswersCorrect = new boolean[amountOfPictures];
+        preSetAnswerToFalse();
+    }
+
+    private void preSetAnswerToFalse() {
+        for(int i = 0; i < areAnswersCorrect.length; i++) {
+            areAnswersCorrect[i] = false;
+        }
+    }
+
+    public void updateAnswers(int currentPicturePositionInALevel, boolean wasAnswerCorrect) {
+        boolean updated = setNewCorrectAnswer(currentPicturePositionInALevel, wasAnswerCorrect);
+        Log.d("LEVELINFOS", " answer was updated in the booleanArray " + updated );
+    }
+
+    private boolean setNewCorrectAnswer(int position, boolean answer) {
+        if(position == getCurrentLevelClicks()) {
+            areAnswersCorrect[position] = answer;
+        } else {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean[] getAreAnswersCorrect() {
+        return areAnswersCorrect;
+    }
+
+    public void setAreAnswersCorrect(boolean[] areAnswersCorrect) {
+        this.areAnswersCorrect = areAnswersCorrect;
     }
 
     public int getLevel() {
@@ -26,5 +61,13 @@ public class Level {
 
     public void setScore(int score) {
         this.score = score;
+    }
+
+    public int getCurrentLevelClicks() {
+        return currentLevelClicks;
+    }
+
+    public void setCurrentLevelClicks(int currentLevelClicks) {
+        this.currentLevelClicks = currentLevelClicks;
     }
 }
