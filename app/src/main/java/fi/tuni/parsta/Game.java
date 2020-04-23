ff094984d46158f9600ba6dfb01933eab70f34f1
Hanna Tuominen <hanna.h.tuominen@tuni.fi>
2020-04-23 17:52:02 +0300

    Make buttons fit screen better.

--- app/src/main/java/fi/tuni/parsta/Game.java
+++ app/src/main/java/fi/tuni/parsta/Game.java
@@ -331,7 +331,7 @@ public class Game extends AppCompatActivity {
                     LinearLayout.LayoutParams.WRAP_CONTENT,
                     LinearLayout.LayoutParams.WRAP_CONTENT
             );
-            params.setMargins(0, 0, 0, 15);
+            params.setMargins(0, 2, 0, 10);
             myButton.setLayoutParams(params);
         }
     }
